82bc1a9a6e84ff00d6c007a1353b8421acbb2faa
Saibal De <saibal.de.bnk@gmail.com>
2020-06-20 01:02:36 -0400

    Add some basic documentation to tt-vector

--- include/tt/vector.hpp
+++ include/tt/vector.hpp
@@ -5,27 +5,61 @@
 
 namespace tt {
 
+/**
+ * TT representation of a multidimensional vector
+ */
 class Vector {
  public:
+  /**
+   * Default constructor.
+   */
   Vector() = default;
 
+  /**
+   * Construct a TT-vector from the dimensions and TT ranks. The cores are all
+   * initialized to zeros.
+   */
   Vector(const arma::Col<arma::uword> &dims,
          const arma::Col<arma::uword> &ranks);
 
+  /**
+   * Construct a TT-vector from the cores.
+   */
   Vector(const arma::field<arma::Cube<double>> &cores);
 
+  /**
+   * Default destructor.
+   */
   ~Vector() = default;
 
+  /**
+   * Return the dimensionality of the TT-vector.
+   */
   arma::uword NumDims() const { return ndim_; }
 
+  /**
+   * Return the size of the TT-vector.
+   */
   const arma::Col<arma::uword> &Dims() const { return dims_; }
 
+  /**
+   * Return the TT-ranks of the TT-vector.
+   */
   const arma::Col<arma::uword> &Ranks() const { return ranks_; }
 
+  /**
+   * Return the maximum TT-rank of the TT-vector.
+   */
   arma::uword MaxRank() const { return maxrank_; }
 
+  /**
+   * Return the specified core of the TT-vector.
+   */
   const arma::Cube<double> &Core(arma::uword i) const { return cores_(i); }
 
+  /**
+   * Compute and return the entry of the TT-vector at given index.
+   */
   double operator()(const arma::Col<arma::uword> &index) const;
 
  private:
