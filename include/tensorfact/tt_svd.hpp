8ad6af5bdc07a1794243fee4b4e2b83b34c81f53
Saibal De <saibal.de.bnk@gmail.com>
2022-03-05 19:32:03 -0800

    Remove TtSvd based on only max_rank specification

--- include/tensorfact/tt_svd.hpp
+++ include/tensorfact/tt_svd.hpp
@@ -3,6 +3,7 @@
 #ifndef TENSORFACT_TTSVD_HPP
 #define TENSORFACT_TTSVD_HPP
 
+#include <limits>
 #include <vector>
 
 #include "tensorfact/tt_tensor.hpp"
@@ -12,24 +13,21 @@ namespace tensorfact {
 /// @brief Compute TT factorization from full tensors using TT-SVD
 ///
 /// Given full tensor \f$\mathcal{X}\f$ of size \f$\{n_0, \ldots, n_{d - 1}\}\f$
-/// and tolerance \f$\tau\f$, compute TT tensor \f$\mathcal{Y}\f$ with optimal
-/// ranks such that \f$\Vert \mathcal{X} - \mathcal{Y} \Vert_2 \leq \tau \Vert
-/// \mathcal{X} \Vert_2\f$.
+/// and tolerance \f$\tau\f$, compute TT tensor \f$\mathcal{Y}\f$ satisfying
+/// \f$\Vert \mathcal{X} - \mathcal{Y} \Vert_2 \leq \tau \Vert \mathcal{X}
+/// \Vert_2\f$ unless limited by specified maximum rank \f$r_{max}\f$.
 ///
 /// @param [in] size Size vector \f$\{n_0, \ldots, n_{d - 1}\}\f$ of full tensor
 /// @param [in] array Full tensor \f$\mathcal{X}\f$ vectorized in column-major
 /// fashion
 /// @param [in] relative_tolerance  Relative tolerance \f$\tau\f$
+/// @param [in] max_rank Maximum TT rank \f$r_{max}\f$ (default: `LONG_MAX`)
 ///
 /// @return TT tensor \f$\mathcal{Y}\f$
 template <typename Real>
 TtTensor<Real> TtSvd(const std::vector<long> &size,
-                     const std::vector<Real> &array, Real relative_tolerance);
-
-/// Compute TT factorization from full tensors using TT-SVD
-template <typename Real>
-TtTensor<Real> TtSvd(const std::vector<long> &size,
-                     const std::vector<Real> &array, long max_rank);
+                     const std::vector<Real> &array, Real relative_tolerance,
+                     long max_rank = std::numeric_limits<long>::max());
 
 }  // namespace tensorfact
 
--- src/tt_svd.cpp
+++ src/tt_svd.cpp
@@ -9,7 +9,8 @@
 template <typename Real>
 tensorfact::TtTensor<Real> tensorfact::TtSvd(const std::vector<long> &size,
                                              const std::vector<Real> &array,
-                                             Real relative_tolerance) {
+                                             Real relative_tolerance,
+                                             long max_rank) {
     if (relative_tolerance < std::numeric_limits<Real>::epsilon()) {
         throw std::logic_error("Specified relative tolerance is too small");
     }
@@ -32,7 +33,8 @@ tensorfact::TtTensor<Real> tensorfact::TtSvd(const std::vector<long> &size,
     std::vector<arma::Cube<Real>> core(ndim);
 
     for (long d = 0; d < ndim - 1; ++d) {
-        arma::Mat<Real> A(array_copy.data(), rank[d] * size[d], array_copy.size() / (rank[d] * size[d]));
+        arma::Mat<Real> A(array_copy.data(), rank[d] * size[d],
+                          array_copy.size() / (rank[d] * size[d]));
 
         arma::Mat<Real> U;
         arma::Col<Real> s;
@@ -40,73 +42,19 @@ tensorfact::TtTensor<Real> tensorfact::TtSvd(const std::vector<long> &size,
         long r;
         TruncatedSvd<Real>(A, delta, true, U, s, V, r);
 
-        core[d] = arma::Cube<Real>(U.memptr(), rank[d], size[d], r);
-
-        arma::Mat<Real> temp = arma::diagmat(s) * V.t();
-        array_copy = std::vector<Real>(temp.memptr(), temp.memptr() + temp.n_elem);
-
-        rank[d + 1] = r;
-    }
-
-    core[ndim - 1] = arma::Cube<Real>(array_copy.data(), rank[ndim - 1], size[ndim - 1], 1);
-    rank[ndim] = 1;
-
-    // construct TT tensor
-    tensorfact::TtTensor<Real> tt_tensor(ndim, size, rank);
-    for (long d = 0; d < ndim; ++d) {
-        for (long k = 0; k < rank[d + 1]; ++k) {
-            for (long j = 0; j < size[d]; ++j) {
-                for (long i = 0; i < rank[d]; ++i) {
-                    tt_tensor.Param(i, j, k, d) = core[d](i, j, k);
-                }
-            }
-        }
-    }
-
-    return tt_tensor;
-}
-
-template <typename Real>
-tensorfact::TtTensor<Real> tensorfact::TtSvd(const std::vector<long> &size,
-                                             const std::vector<Real> &array,
-                                             long max_rank) {
-    if (max_rank < 1) {
-        throw std::logic_error("Maximum rank must be at least 1");
-    }
-
-    long ndim = size.size();
-    std::vector<long> rank(ndim + 1);
-
-    // make copy of array
-    std::vector<Real> array_copy(array);
-
-    // compute cores
-    rank[0] = 1;
-    std::vector<arma::Cube<Real>> core(ndim);
-
-    for (long d = 0; d < ndim - 1; ++d) {
-        arma::Mat<Real> A(array_copy.data(), rank[d] * size[d], array_copy.size() / (rank[d] * size[d]));
-
-        arma::Mat<Real> U_temp;
-        arma::Col<Real> s_temp;
-        arma::Mat<Real> V_temp;
-        long r_temp;
-        TruncatedSvd<Real>(A, 0.0, false, U_temp, s_temp, V_temp, r_temp);
-
-        long r = std::min(r_temp, max_rank);
-        arma::Mat<Real> U(U_temp.memptr(), U_temp.n_rows, r);
-        arma::Col<Real> s(s_temp.memptr(), r);
-        arma::Mat<Real> V(V_temp.memptr(), V_temp.n_rows, r);
+        r = std::min(r, max_rank);
 
         core[d] = arma::Cube<Real>(U.memptr(), rank[d], size[d], r);
 
         arma::Mat<Real> temp = arma::diagmat(s) * V.t();
-        array_copy = std::vector<Real>(temp.memptr(), temp.memptr() + temp.n_elem);
+        array_copy =
+            std::vector<Real>(temp.memptr(), temp.memptr() + temp.n_elem);
 
         rank[d + 1] = r;
     }
 
-    core[ndim - 1] = arma::Cube<Real>(array_copy.data(), rank[ndim - 1], size[ndim - 1], 1);
+    core[ndim - 1] =
+        arma::Cube<Real>(array_copy.data(), rank[ndim - 1], size[ndim - 1], 1);
     rank[ndim] = 1;
 
     // construct TT tensor
@@ -128,14 +76,7 @@ tensorfact::TtTensor<Real> tensorfact::TtSvd(const std::vector<long> &size,
 
 template tensorfact::TtTensor<float> tensorfact::TtSvd(
     const std::vector<long> &size, const std::vector<float> &array,
-    float relative_tolerance);
+    float relative_tolerance, long max_rank);
 template tensorfact::TtTensor<double> tensorfact::TtSvd(
     const std::vector<long> &size, const std::vector<double> &array,
-    double relative_tolerance);
-
-template tensorfact::TtTensor<float> tensorfact::TtSvd(
-    const std::vector<long> &size, const std::vector<float> &array,
-    long max_rank);
-template tensorfact::TtTensor<double> tensorfact::TtSvd(
-    const std::vector<long> &size, const std::vector<double> &array,
-    long max_rank);
+    double relative_tolerance, long max_rank);
